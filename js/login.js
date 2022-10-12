d48d78cfea16887b2ce9e0622026051cd8493ee1
ruizjose545 <ruizj@gmkt.local>
2022-10-11 21:15:13 -0300

    trabajamos en login.js

--- js/login.js
+++ js/login.js
@@ -4,26 +4,83 @@ let inputUser = document.getElementById("inputUser");
 let inputPass = document.getElementById("inputPass");
 let formLogin = document.getElementById("formLogin");
 let sesionInit = false;
-
+let ls = window.localStorage
 //asociando eventos
+// inputUser.addEventListener("blur", () => {
+//     validarCampos(inputUser);
+// });
+// inputPass.addEventListener("blur", () => {
+//   validatePass(inputPass);
+// });
+
 inputUser.addEventListener("blur", () => {
-    validarCampos(inputUser);
+  validarCampos(user);
 });
 inputPass.addEventListener("blur", () => {
-  validatePass(inputPass);
+validatePass(pass);
 });
-formLogin.addEventListener("submit", login);
 
-//funciones
+//formLogin.addEventListener("submit", login);
+
+// //funciones
+
+// function login(e) {
+//   e.preventDefault();
+//   if (gralValidate(user, pass)) {
+//     if (user.value === usuarioAdmin.name) {
+//       if (pass.value === usuarioAdmin.password) {
+//         sesionInit = true;
+//         // sessionStorage.setItem("stateSesion", JSON.stringify(sesionInit));
+//         //localStorage.setItem("stateSesion",JSON.stringify(sesionInit))
+//         //localStorage.setItem("user", JSON.stringify(superUser));
+//         // window.location.replace("index.html");
+//         console.log("usuario admin");  
+//         alert("Su usuario es administrador");      
+        
+//       } else {
+//         alert("Contraseña Incorrecta. Ingrese nuevamente");
+//       }
+//     } else if (user.value === findUser(inputUser.value).usuario) {
+//       if (pass.value === findUser(inputUser.value).contraseña) {
+//         let newUser = findUser(inputUser.value);
+//         sesionInit = true;
+//         //sessionStorage.setItem("stateSesion", JSON.stringify(sesionInit));
+//         localStorage.setItem("stateSesion",JSON.stringify(sesionInit))
+//         localStorage.setItem("user", JSON.stringify(newUser));
+//         //window.location.replace("index.html");
+//       } else {
+//         alert("Contraseña Incorrecta. Ingrese nuevamente");
+//       }
+//     } else {
+//       alert("Usuario Incorrecto. Ingrese nuevamente");
+//     }
+//   } else {
+//     alert("Debe completar todos los campos");
+//   }
+// }
+
+// function findUser(usuario) {
+//   let newUser = regUser.find((user) => {
+//     return user.usuario === usuario;
+//   });
+//   if (newUser !== undefined) {
+//     return newUser;
+//   } else {
+//     return "";
+//   }
+// }
+
+
 
 function login(e) {
   e.preventDefault();
-  if (gralValidate(inputUser, inputPass)) {
-    if (inputUser.value === superUser.name) {
-      if (inputPass.value === superUser.password) {
+  if (gralValidate(user, pass)) {
+    if (user.value === input_User.value) {
+      if (pass.value === input_Pass.value) {
         sesionInit = true;
         // sessionStorage.setItem("stateSesion", JSON.stringify(sesionInit));
-        localStorage.setItem("user", JSON.stringify(superUser));
+        //localStorage.setItem("stateSesion",JSON.stringify(sesionInit))
+        //localStorage.setItem("user", JSON.stringify(superUser));
         // window.location.replace("index.html");
         console.log("usuario admin");  
         alert("Su usuario es administrador");      
@@ -31,14 +88,15 @@ function login(e) {
       } else {
         alert("Contraseña Incorrecta. Ingrese nuevamente");
       }
-    } else if (inputUser.value === findUser(inputUser.value).usuario) {
-      if (inputPass.value === findUser(inputUser.value).contraseña) {
-        let newUser = findUser(inputUser.value);
-        sesionInit = true;
-        //sessionStorage.setItem("stateSesion", JSON.stringify(sesionInit));
-        localStorage.setItem("stateSesion",JSON.stringify(sesionInit))
-        localStorage.setItem("user", JSON.stringify(newUser));
-        window.location.replace("index.html");
+    } else if (user.value === regUser.usuario) {
+      if (pass.value === regUser.contraseña) {
+        alert("Su usuario esta en ls");      
+        // let newUser = findUser(inputUser.value);
+        // sesionInit = true;
+        // //sessionStorage.setItem("stateSesion", JSON.stringify(sesionInit));
+        // localStorage.setItem("stateSesion",JSON.stringify(sesionInit))
+        // localStorage.setItem("user", JSON.stringify(newUser));
+        // //window.location.replace("index.html");
       } else {
         alert("Contraseña Incorrecta. Ingrese nuevamente");
       }
@@ -50,47 +108,56 @@ function login(e) {
   }
 }
 
-function findUser(usuario) {
-  let newUser = regUser.find((user) => {
-    return user.usuario === usuario;
-  });
-  if (newUser !== undefined) {
-    return newUser;
-  } else {
-    return "";
-  }
-}
 
-//validaciones
+// //validaciones
 
-    function validarCampos(input) {
-        if (input.value.trim().length > 0) {
-          input.className = "form-control is-valid";
+    function validarCampos(user) {
+        if (user.value.trim().length > 0) {
+          user.className = "form-control is-valid";
           return true;
         } else {        {
-            input.className = "form-control is-invalid";
+           user.className = "form-control is-invalid";
             return false;
           }
         }
     }
 
-function validatePass(input) {
+function validatePass(pass) {
   let regPass = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/;
-  if (regPass.test(input.value)) {
+  if (regPass.test(pass.value)) {
     input.className = "form-control is-valid";
     return true;
-  } else if (input.value.trim().length > 0 && input.value.trim().length < 9) {
-    input.className = "form-control is-valid";
+  } else if (pass.value.trim().length > 0 && pass.value.trim().length < 9) {
+    pass.className = "form-control is-valid";
     return true;
   } else {
-    inputPass.className = "form-control is-invalid";
+    pass.className = "form-control is-invalid";
   }
 }
 
-function gralValidate(inputUser, inputPass) {
-  if (validarCampos(inputUser) && validatePass(inputPass)) {
+function gralValidate(user, pass) {
+  if (validarCampos(user) && validatePass(pass)) {
     return true;
   } else {
     return false;
   }
 }
+
+
+//-------------------------------------------------------
+
+inputUser.addEventListener("focusout",function(){
+ls.setItem("user",inputUser.value);
+
+})
+
+inputPass.addEventListener("focusout",function(){
+  ls.setItem("pass",inputPass.value);
+  
+  })
+
+function recuperarValores(){
+  inputUser.value = ls.getItem("user");
+  inputPass.value = ls.getItem("pass");
+}
+document.addEventListener("DOMContentLoaded",recuperarValores)
