6ff248b47e4eb947cb22cdb206f4d7ff3caf33c0
Federico Valdiglesias <78738426+FedericoVM@users.noreply.github.com>
2022-10-14 14:49:50 -0300

    Pagina principal (#22)
    * correccion de detalles del navbar
    
    * agregue funcionalidad al index
    
    * corrrecion de funcionalidad

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 
 class Producto {
     constructor(nombre, marca, categoria, precio, url) {
+
         this.nombre = nombre;
         this.marca = marca;
         this.categoria = categoria;
@@ -27,13 +28,10 @@ let div_3 = document.querySelector(".div-3");
 
 
 
-
-
-
-
 // Crear las instancias y guardar en un arrray
 
 let listaProductos = [
+
     // Computacion
     new Producto("Auricular", "Sony", "Computacion", "600", "https://s3-us-west-2.amazonaws.com/assets.ximaro.net/pictures/files/000/004/899/original/AURICULAR-SONY-PLEGABLES-02.jpg?1646944717"),
     new Producto("Teclado", "Redragon", "Computacion", "4.000", "https://s3-us-west-2.amazonaws.com/assets.ximaro.net/pictures/files/000/003/348/original/Rd-K503-HarpePro.jpg?1618851847"),
@@ -48,7 +46,6 @@ let listaProductos = [
     new Producto("Batidora de mano", "Philips", "Electrodomesticos", "5.400", "https://images.fravega.com/f300/97c9030e15927e70802ba8778d607f27.jpg.webp"),
     new Producto("Pava electricas", "Philips", "Electrodomesticos", "10.500", "https://images.fravega.com/f300/054e150e13a3fa37f0ef8b381b5f1646.jpg.webp"),
 
-
     // Aire Libre
     new Producto("Carpa", "Coleman", "Aire Libre", "45.600", "https://www.canigo.com.ar/wp-content/uploads/11514_1.jpg"),
     new Producto("Caña de pescar", "Maquieira ", "Aire Libre", "9.749", "https://media.istockphoto.com/vectors/spinning-for-fishing-flat-isolated-vector-id697053732?k=20&m=697053732&s=612x612&w=0&h=VVT6qW91dofUsiBBJrnmKHh-tUmLz5sQqPKZDc6peDc="),
@@ -56,36 +53,109 @@ let listaProductos = [
     new Producto("Carpa", "Coleman", "Aire Libre", "45.600", "https://www.canigo.com.ar/wp-content/uploads/11514_1.jpg"),
     new Producto("Termo", "Stanley", "Aire Libre", "10.600", "https://images.fravega.com/f300/555b8b4a73df0cfd391d6fb488050e1b.jpg.webp")
 
-
 ];
 
 
 
 
-// Guardar la lista en el local storage
+// // Guardar la lista en el local storage
 
-  localStorage.setItem("listaProductos", JSON.stringify(listaProductos));
+localStorage.setItem("listadoProductos", JSON.stringify(listaProductos));
 
 
-// Traer la lista de productos desde el local Storage
+// // Traer la lista de productos desde el local Storage
 
-listaProductos = JSON.parse(localStorage.getItem(`listaProductos`));
+let listaProductosDos = JSON.parse(localStorage.getItem(`listadoProductos`));
 
-// Filtro los productos de cada categoria por listas separadas
-let productosComputacion = listaProductos.filter(producto => producto.categoria == "Computacion");
-let productosElectrodomesticos = listaProductos.filter(producto => producto.categoria == "Electrodomesticos");
-let productosAireLibre = listaProductos.filter(producto => producto.categoria == "Aire Libre");
+// // Filtro los productos de cada categoria por listas separadas
+let productosComputacion = listaProductosDos.filter(producto => producto.categoria == "Computacion");
+let productosElectrodomesticos = listaProductosDos.filter(producto => producto.categoria == "Electrodomesticos");
+let productosAireLibre = listaProductosDos.filter(producto => producto.categoria == "Aire Libre");
 
 function cargarProducto() {
 
-    if (productosComputacion.length <= 0) {
-        div_1.style.display="none";
+    if (productosComputacion.length === 0) {
+        div_1.style.display = "none";
     } else {
+        if (productosComputacion.length < 4 ) {
+            productosComputacion.forEach(producto => {
+                
+                div_computacion.innerHTML += `
+        
+                <div class="card md-3  mx-lg-3  p-lg-0  mt-3 shadow" style="max-width: 540px;">
+                <div class="row g-0 align-items-center d-lg-flex flex-lg-column">
+                    <div class="col-md-4 col-5 col-lg-7 d-flex align-items-center  h-75">
+                        <img src="${producto.url}"
+                            class="imagen-producto img-fluid rounded-start" alt="imagen-producto">
+                    </div>
+                    <div class="col-md-8 col-7">
+                        <div class="card-body d-flex flex-column align-items-center px-sm-0 py-sm-2  ">
+                            <a href="http://">
+                                <h6 class="card-title">${producto.nombre}</h6>
+                            </a>
+                            <p class="m-0"><small>Marca: ${producto.marca}</small></p>
+                            <p class="m-0"><small>Categoria: ${producto.categoria}</small></p>
+                            <h5 class="card-text fw-bold m-0 text-center mt-2 ">$ ${producto.precio}</h5>
+                            <div class=" d-flex flex-column align-items-center mt-3 row " >
+                                <a class="btn-carrito btn btn-light btn-sm mt-2 col-8 w-100" href="#" role="button" ><img
+                                        class="carrito" src="./img/index/pagina-principal/card/cart3.svg"
+                                        alt=""></a>
+                                <a class="btn-comprar  btn btn-primary btn-sm mt-2 col-8 w-100" href="#"
+                                    role="button">Comprar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </di>
+            
+                `
+            });
+        } else {
+            for (let i = productosComputacion.length - 1; i > (productosComputacion.length - 1) - 3; i--) {
+                let p = productosComputacion[i];
+                div_computacion.innerHTML += `
+        
+                <div class="card md-3  mx-lg-3  p-lg-0  mt-3 shadow" style="max-width: 540px;">
+                <div class="row g-0 align-items-center d-lg-flex flex-lg-column">
+                    <div class="col-md-4 col-5 col-lg-7 d-flex align-items-center  h-75">
+                        <img src="${p.url}"
+                            class="imagen-producto img-fluid rounded-start" alt="imagen-producto">
+                    </div>
+                    <div class="col-md-8 col-7">
+                        <div class="card-body d-flex flex-column align-items-center px-sm-0 py-sm-2  ">
+                            <a href="http://">
+                                <h6 class="card-title">${p.nombre}</h6>
+                            </a>
+                            <p class="m-0"><small>Marca: ${p.marca}</small></p>
+                            <p class="m-0"><small>Categoria: ${p.categoria}</small></p>
+                            <h5 class="card-text fw-bold m-0 text-center mt-2 ">$ ${p.precio}</h5>
+                            <div class=" d-flex flex-column align-items-center mt-3 row " >
+                                <a class="btn-carrito btn btn-light btn-sm mt-2 col-8 w-100" href="#" role="button" ><img
+                                        class="carrito" src="./img/index/pagina-principal/card/cart3.svg"
+                                        alt=""></a>
+                                <a class="btn-comprar  btn btn-primary btn-sm mt-2 col-8 w-100" href="#"
+                                    role="button">Comprar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </di>
+            
+                `
+            }
+        }       
 
-        for (let i = productosComputacion.length-1; i > (productosComputacion.length-1)-3; i--) {
-            const producto = productosComputacion[i];
-            div_computacion.innerHTML += `
-    
+    }
+
+
+    if (productosElectrodomesticos.length === 0) {
+
+        div_2.style.display = "none"
+    } else {
+        if(productosElectrodomesticos.length < 4) {
+            productosElectrodomesticos.forEach(producto => {
+                div_electrodomesticos.innerHTML += `
+     
             <div class="card md-3  mx-lg-3  p-lg-0  mt-3 shadow" style="max-width: 540px;">
             <div class="row g-0 align-items-center d-lg-flex flex-lg-column">
                 <div class="col-md-4 col-5 col-lg-7 d-flex align-items-center  h-75">
@@ -101,6 +171,7 @@ function cargarProducto() {
                         <p class="m-0"><small>Categoria: ${producto.categoria}</small></p>
                         <h5 class="card-text fw-bold m-0 text-center mt-2 ">$ ${producto.precio}</h5>
                         <div class=" d-flex flex-column align-items-center mt-3 row " >
+
                             <a class="btn-carrito btn btn-light btn-sm mt-2 col-8 w-100" href="#" role="button" ><img
                                     class="carrito" src="./img/index/pagina-principal/card/cart3.svg"
                                     alt=""></a>
@@ -111,26 +182,14 @@ function cargarProducto() {
                 </div>
             </div>
         </di>
-        
-        
-            `  
-        }
+                      
+            `
+            });
+            
+        } else {
+            
+        for (let i = productosElectrodomesticos.length - 1; i > (productosElectrodomesticos.length - 1) - 3; i--) {
 
-        // btn_ver_mas_c.style.display="block";
-
-    }
-
-
-
-
-
-
-    if (productosElectrodomesticos.length <= 0) {
-
-        div_2.style.display="none"
-    } else {
-
-        for (let i = productosElectrodomesticos.length-1; i > (productosElectrodomesticos.length-1)-3; i--) {
             const producto = productosElectrodomesticos[i];
             div_electrodomesticos.innerHTML += `
     
@@ -162,7 +221,10 @@ function cargarProducto() {
             
             
             `
-           
+
+        }
+
+
         }
 
         // btn_ver_mas_e.style.display="block";
@@ -170,54 +232,82 @@ function cargarProducto() {
     }
 
 
+    if (productosAireLibre.length === 0) {
 
-
-
-
-
-
-    if (productosAireLibre.length <= 0) {
-
-        div_3.style.display="none";
+        div_3.style.display = "none";
     } else {
-
-        for (let i = productosAireLibre.length-1; i > (productosAireLibre.length-1)-3; i--) {
-            const producto = productosAireLibre[i];
-            div_aire_libre.innerHTML += `
-    
-            <div class="card md-3  mx-lg-3  p-lg-0  mt-3 shadow" style="max-width: 540px;">
-            <div class="row g-0 align-items-center d-lg-flex flex-lg-column">
-                <div class="col-md-4 col-5 col-lg-7 d-flex align-items-center  h-75">
-                    <img src="${producto.url}"
-                        class="imagen-producto img-fluid rounded-start" alt="imagen-producto">
-                </div>
-                <div class="col-md-8 col-7">
-                    <div class="card-body d-flex flex-column align-items-center px-sm-0 py-sm-2  ">
-                        <a href="http://">
-                            <h6 class="card-title">${producto.nombre}</h6>
-                        </a>
-                        <p class="m-0"><small>Marca: ${producto.marca}</small></p>
-                        <p class="m-0"><small>Categoria: ${producto.categoria}</small></p>
-                        <h5 class="card-text fw-bold m-0 text-center mt-2 ">$ ${producto.precio}</h5>
-                        <div class=" d-flex flex-column align-items-center mt-3 row " >
-                            <a class="btn-carrito btn btn-light btn-sm mt-2 col-8 w-100" href="#" role="button" ><img
-                                    class="carrito" src="./img/index/pagina-principal/card/cart3.svg"
-                                    alt=""></a>
-                            <a class="btn-comprar  btn btn-primary btn-sm mt-2 col-8 w-100" href="#"
-                                role="button"  >Comprar</a>
+        if(productosAireLibre.length < 4) {
+            productosAireLibre.forEach(producto => {
+                div_aire_libre.innerHTML += `
+        
+                <div class="card md-3  mx-lg-3  p-lg-0  mt-3 shadow" style="max-width: 540px;">
+                <div class="row g-0 align-items-center d-lg-flex flex-lg-column">
+                    <div class="col-md-4 col-5 col-lg-7 d-flex align-items-center  h-75">
+                        <img src="${producto.url}"
+                            class="imagen-producto img-fluid rounded-start" alt="imagen-producto">
+                    </div>
+                    <div class="col-md-8 col-7">
+                        <div class="card-body d-flex flex-column align-items-center px-sm-0 py-sm-2  ">
+                            <a href="http://">
+                                <h6 class="card-title">${producto.nombre}</h6>
+                            </a>
+                            <p class="m-0"><small>Marca: ${producto.marca}</small></p>
+                            <p class="m-0"><small>Categoria: ${producto.categoria}</small></p>
+                            <h5 class="card-text fw-bold m-0 text-center mt-2 ">$ ${producto.precio}</h5>
+                            <div class=" d-flex flex-column align-items-center mt-3 row " >
+                                <a id="link-carrito" class="btn-carrito btn btn-light btn-sm mt-2 col-8 w-100" href="#" role="button" ><img
+                                        class="carrito" src="./img/index/pagina-principal/card/cart3.svg"
+                                        alt=""></a>
+                                <a class="btn-comprar  btn btn-primary btn-sm mt-2 col-8 w-100" href="#"
+                                    role="button"  >Comprar</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </di>
-            
-            
-            `
-           
+            </di>
+                
+                
+                `
+            });
+          
+        } else {
+            for (let i = productosAireLibre.length - 1; i > (productosAireLibre.length - 1) - 3; i--) {
+                let producto = productosAireLibre[i];
+                div_aire_libre.innerHTML += `
+        
+                <div class="card md-3  mx-lg-3  p-lg-0  mt-3 shadow" style="max-width: 540px;">
+                <div class="row g-0 align-items-center d-lg-flex flex-lg-column">
+                    <div class="col-md-4 col-5 col-lg-7 d-flex align-items-center  h-75">
+                        <img src="${producto.url}"
+                            class="imagen-producto img-fluid rounded-start" alt="imagen-producto">
+                    </div>
+                    <div class="col-md-8 col-7">
+                        <div class="card-body d-flex flex-column align-items-center px-sm-0 py-sm-2  ">
+                            <a href="http://">
+                                <h6 class="card-title">${producto.nombre}</h6>
+                            </a>
+                            <p class="m-0"><small>Marca: ${producto.marca}</small></p>
+                            <p class="m-0"><small>Categoria: ${producto.categoria}</small></p>
+                            <h5 class="card-text fw-bold m-0 text-center mt-2 ">$ ${producto.precio}</h5>
+                            <div class=" d-flex flex-column align-items-center mt-3 row " >
+                                <a id="link-carrito" class="btn-carrito btn btn-light btn-sm mt-2 col-8 w-100" href="#" role="button" ><img
+                                        class="carrito" src="./img/index/pagina-principal/card/cart3.svg"
+                                        alt=""></a>
+                                <a class="btn-comprar  btn btn-primary btn-sm mt-2 col-8 w-100" href="#"
+                                    role="button"  >Comprar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </di>
+                
+                
+                `
+    
+            }
+          
         }
-        // btn_ver_mas_a.style.display="block";
-
-
+       
     }
 
 
@@ -225,3 +315,4 @@ function cargarProducto() {
 
 
 cargarProducto();
+
